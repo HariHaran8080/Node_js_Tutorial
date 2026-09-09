3b0a49759d173a17fe907ab05cea9dfbc73b2372
Hari <haritamil2912@gmail.com>
2026-09-09 12:15:06 +0530

    File Systems

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-const fs=require('fs')
 const path=require('path')
+const fsPromises=require('fs').promises
 
-fs.readFile(path.join(__dirname,'files','start.txt'), 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+const fileOps=async()=>
+{
+  try{
+      const data = await fsPromises.readFile(path.join(__dirname, "files", "start.txt"),"utf8");
+      console.log(data);
+      await fsPromises.writeFile(path.join(__dirname, "files", "Sunshine.txt"), "If you can't do it Then who can?")
+      console.log('writing complete');
+      await fsPromises.appendFile(path.join(__dirname, "files", "sunshine.txt"), "\n\n then If you can't do it Then who can?")
+      console.log('append complete');
+      await fsPromises.rename(path.join(__dirname, "files", "sunshine.txt"),(path.join(__dirname,"files",'darkshine.txt')))
+      console.log("successfully renamed the file");
+      await fsPromises.unlink(path.join(__dirname, "files", "start.txt"));
+    }catch(err){
+    console.error(err);
+  }
+}
+fileOps();
 
-fs.writeFile(path.join(__dirname, "files", "sunshine.txt"), "If you can't do it Then who can?", (err, data) => {
-  if (err) throw err;
-  console.log('work done');
-});
-
-fs.appendFile(path.join(__dirname, "files", "sunshine.txt"), "\n\n then If you can't do it Then who can?", (err, data) => {
-  if (err) throw err;
-  console.log('append complete');
-});
-
+// Exit on uncaught error
 process.on('uncaughtException',err=>{
   console.error(`there was an uncaught error :${err}`)
   process.exit(1)
